6bcb44348e7e189b3dd746b751131f713e6c9209
Naveen <chinna143na@gmail.com>
2025-05-03 21:40:48 -0400

    Add project files.

---
+++ Models/DataAccess/CareSyncDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CareSync.Models.DataAccess
+{
+    public class CareSyncDbContext : DbContext
+    {
+        public CareSyncDbContext(DbContextOptions<CareSyncDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
